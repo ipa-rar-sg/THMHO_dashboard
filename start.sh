5b471325f36369e7b485258ccf23d9c36a5643aa
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2021-09-16 13:53:53 +0200

    added a docker file to build ROS apps to docker-compose

--- start.sh
+++ start.sh
@@ -16,11 +16,11 @@ echo "INPUT WIDTH:"
 read TABLE_WIDTH
 echo "INPUT HEIGHT:"
 read TABLE_HEIGHT
-echo "MONGO_COLLECTION=$MONGO_COLLECTION" > ../docker/.env
-echo "HEATMAP_WIDTH=$TABLE_WIDTH" >> ../docker/.env
-echo "HEATMAP_HEIGHT=$TABLE_HEIGHT" >> ../docker/.env
-docker-compose -f ../docker/docker-compose.yml up --build -d
+echo "MONGO_COLLECTION=$MONGO_COLLECTION" > ./docker/.env
+echo "HEATMAP_WIDTH=$TABLE_WIDTH" >> ./docker/.env
+echo "HEATMAP_HEIGHT=$TABLE_HEIGHT" >> ./docker/.env
+docker-compose -f ./docker/docker-compose.yml up --build -d
 echo "FINISHED TABLE CONFIGURATION"
 echo "----------------------------------------"
-docker-compose -f ../docker/docker-compose.yml logs -f
+docker-compose -f ./docker/docker-compose.yml logs -f
 
