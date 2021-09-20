27b96e923d51214ff32084048e4baeabb0fca234
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2021-09-20 12:44:41 +0200

    updated docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Source ROS distro environment and local catwin workspace
+source "/opt/ros/noetic/setup.bash" && source "$CATKIN_WS/devel/setup.bash"
+
+#roslaunch thmho_firos_heatmap_bringup docker_bringup.launch
