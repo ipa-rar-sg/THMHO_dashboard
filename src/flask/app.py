f4bf56e1cce0cc2ce36aa01bcafd713187e3c7ab
Sebastian Gomez Ruiz <sebastian.gomez.ruiz@ipa.fraunhofer.de>
2021-11-22 17:56:36 +0100

    Flask updated for comparing mongo with sparse and no sparse

--- src/flask/app.py
+++ src/flask/app.py
@@ -10,14 +10,20 @@ config = {
     'dbname' : os.getenv('MONGO_INITDB_DATABASE'),
     'user' : os.getenv('MONGO_INITDB_ROOT_USERNAME'),
     'pass' : os.getenv('MONGO_INITDB_ROOT_PASSWORD'),
-    'host' : 'mongo'
+    'host_no_csr' : 'mongo',
+    'host_csr': 'mongocsr'
 }
 
-conn_str = f"mongodb://{config['user']}:{config['pass']}@{config['host']}:27017/{config['dbname']}"
+conn_str = f"mongodb://{config['user']}:{config['pass']}@{config['host_no_csr']}:27017/{config['dbname']}"
+conn_str_csr = f"mongodb://{config['user']}:{config['pass']}@{config['host_csr']}:27018/{config['dbname']}"
 
 conn = pymongo.MongoClient(conn_str, connect=False)
 conn_db = conn[config['dbname']]
 conn_col = conn_db[config['collection']]
+
+conn_csr = pymongo.MongoClient(conn_str_csr, connect=False)
+conn_db_csr = conn_csr[config['dbname']]
+conn_col_csr = conn_db_csr[config['collection']]
 
 @app.route('/')
 def home():
@@ -37,12 +43,12 @@ def insert():
     '''
     body = request.json
     reg_id = conn_col.insert_one(body).inserted_id
-    return f'Inserted successfully registry with new id: {reg_id}'
+    reg_id2 = conn_col_csr.insert_one(body).inserted_id
+    return f'Inserted successfully registry with new id: Mongo: {reg_id} & {reg_id2}'
 
 @app.route('/select', methods=['GET'])
 def select():
     '''
-    Still to implement
     '''
     pass
 
