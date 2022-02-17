51e8ebd1b63bc0085b08cc2155f0ee83f6283a39
Sebastian Gomez Ruiz <sebastian.gomez.ruiz@ipa.fraunhofer.de>
2022-02-17 09:14:13 +0100

    Working version, only missing slider functionality

--- src/dash/utils.py
+++ src/dash/utils.py
@@ -22,7 +22,7 @@ class DataHolder:
     def __init__(self):
         self.config = config
         self.shape = (self.config['height'], self.config['width'])
-        self.timed_data = None
+        self.timed_data = []
         self.last_data = []
         self.set_connection()
         self.read_mask()
@@ -52,7 +52,7 @@ class DataHolder:
         result = self.read_from_query({"name": "base_mask"})
         while not result:
             result = self.read_from_query({"name": "base_mask"})
-        mask = result[0]
+        mask = result[-1]
         csr = self.generate_csr(mask)
         self.mask = csr.toarray()
         self.mask = (~(self.mask.astype(bool))).astype(int)
@@ -73,8 +73,8 @@ class DataHolder:
         if _tmp:
             _tmp = _tmp[len(_tmp) // 2]
             _date = _tmp['date']
-            _data = generate_csr(_tmp)
-            return decode(_data), _date
+            _data = self.generate_csr(_tmp)
+            return self.decode(_data), _date
         return np.zeros(self.shape), f'{date.isoformat()} NOT FOUND'
 
     def get_data_from_date_bunch(self, date, delta=60):
@@ -85,6 +85,6 @@ class DataHolder:
             self.timed_data = _tmp[-10]
 
     def get_last_data(self):
-        _tmp = generate_csr(self.last_data)
-        return decode(_tmp)
+        _tmp = self.generate_csr(self.last_data)
+        return self.decode(_tmp)
 
